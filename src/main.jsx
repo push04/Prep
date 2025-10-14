26b459bad62fec84bdd41a5b3348707a0c2bccde
push04 <pushpal.sanyal_btech24@gsv.ac.in>
2025-10-14 20:30:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles.css"; // Tailwind + component layer
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
